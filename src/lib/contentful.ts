f560714f6985d69b99350735f5eed0baa0007e4d
Alex Grigore <grigore.alex22@gmail.com>
2024-02-13 20:52:02 +0200

    Contentful integration

---
+++ src/lib/contentful.ts
@@ -0,0 +1,41 @@
+import contentful, { type EntryFieldTypes } from "contentful";
+
+export interface BlogPost {
+  contentTypeId: "blogPost";
+  sys: {
+    id: string;
+  };
+  fields: {
+    name: EntryFieldTypes.Text;
+    postBody: EntryFieldTypes.RichText;
+    date: EntryFieldTypes.Date;
+    postImage: EntryFieldTypes.AssetLink;
+  };
+}
+
+export interface ImgType {
+  fields: {
+    title: string;
+    file: {
+      fileName: string;
+      contentType: string;
+      details: {
+        image: {
+          width: number;
+          height: number;
+        };
+        size: number;
+      };
+      url: string;
+    };
+    description: string;
+  };
+}
+
+export const contentfulClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
